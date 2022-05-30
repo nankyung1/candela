ffca2eb90128bb8f7f7fbba39c529d8735ba1331
nankyung1 <105191958+nankyung1@users.noreply.github.com>
2022-05-30 17:58:13 +0900

    Add files via upload

--- js/product.js
+++ js/product.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
 });
 
 $(function() {
-	$('a[href*="#"]:not([href="#"])').click(function() {
+	$('a[href*="#"]:not([href="#"]):not([href="#tab1"]):not([href="#tab2"]):not([href="#tab3"]):not([href="#tab4"])').click(function() {
 	if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 	var target = $(this.hash);
 	target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
